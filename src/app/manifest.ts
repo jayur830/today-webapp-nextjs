9ce209f333a07cab25f98422428f9936d5d60036
jayur830 <jayur830@naver.com>
2025-04-12 14:31:44 +0900

    ci: seo 설정

---
+++ src/app/manifest.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    short_name: 'today',
+    name: '오늘 뭐 입지?',
+    icons: [
+      {
+        src: './favicon.ico',
+        sizes: '64x64 32x32 24x24 16x16',
+        type: 'image/x-icon',
+      },
+      {
+        src: './favicon.ico',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+    ],
+    start_url: '.',
+    display: 'standalone',
+    theme_color: '#FFFFFF',
+    background_color: '#FFFFFF',
+  };
+}
