821b4fb0d2e7f1b80fa4e09a00f88dad11fd7505
jayur830 <jayur830@naver.com>
2025-04-13 20:17:42 +0900

    chore: commit convention 세팅 완료

--- run.sh
+++ run.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
 
-# 깃 저장소 초기화
+# Initialize git repository
 if [ ! -d '.git' ]; then
-  git init
+  echo git init
 fi
 
-# 패키지 락 파일이 없으면 패키지 초기화
-if [ ! -d 'package-lock.json' ]; then
+# Initialize packages if package.json doesn't exist
+if [ ! -e 'package.json' ]; then
   npm init -y
   npm i
 fi
 
-# yarn 사용 시
-if [ -d yarn.lock ]; then
+# For yarn users
+if [ -e yarn.lock ]; then
   yarn add -D husky commitizen @commitlint/cli @commitlint/config-conventional
   yarn husky
   yarn commitizen init cz-conventional-changelog --yarn --dev --exact
 
-# pnpm 사용 시
-elif [ -d pnpm-lock.yaml ]; then
+# For pnpm users
+elif [ -e pnpm-lock.yaml ]; then
   pnpm add -D husky commitizen @commitlint/cli @commitlint/config-conventional
   pnpm husky
   pnpm commitizen init cz-conventional-changelog --pnpm --save-dev --save-exact
 
-# npm 사용 시
-else
+# For npm users
+elif [ -e package-lock.json ]; then
   npm i -D husky commitizen @commitlint/cli @commitlint/config-conventional
   npx husky
   npx commitizen init cz-conventional-changelog --save-dev --save-exact
+
 fi
 
-# package.json 설정
+# Configure package.json
 if [ `grep -c '"prepare": "husky"' package.json` == 0 ]; then
   sed -i '' 's/  "scripts": {/  "scripts": {\n    "prepare": "husky",/g' package.json
 fi
 
-# prepare-commit-msg 파일 생성
+# Create prepare-commit-msg file
 echo "#!/bin/bash
 exec < /dev/tty && node_modules/.bin/cz --hook || true
 " > .husky/prepare-commit-msg
 
 chmod +x .husky/prepare-commit-msg
 
-# commitlint 설정
+# Configure commitlint
 echo "module.exports = { extends: ['@commitlint/config-conventional'] };" > commitlint.config.js
 
 echo "#!/bin/bash
@@ -50,3 +51,6 @@ node_modules/.bin/commitlint --edit "$1"
 " > .husky/commit-msg
 
 chmod +x .husky/commit-msg
+
+# Husky Install
+yarn husky
